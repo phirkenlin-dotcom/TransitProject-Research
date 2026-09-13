ef17d66b7cb9014ba88d4745ba3f1f9285cfcc99
phirkenlin-dotcom <chinphirke@gmail.com>
2026-09-12 17:52:48 -0700

    Update 1_Transit_Research.py

--- pages/1_Transit_Research.py
+++ pages/1_Transit_Research.py
@@ -21,6 +21,15 @@ with col2:
 st.header("2. Geographic Map View")
 st.image("School with Bus Stop Map.jpg", caption="Map of Schools (Blue) and Transit Stops (Orange)")
 
-st.header("3. Focus Area: Schools More Than 0.5 Miles Away")
+# st.header("3. Focus Area: Schools More Than 0.5 Miles Away")
 st.write("These schools face the greatest distance from public transit options.")
 st.image("Schools With Bus Stops More Than 0.5 Miles away by School Name.jpg", caption="Schools with Bus Stops > 0.5 Miles Away")
+
+st.header("Data Sources")
+st.write("For this analysis I used various data which is publically available. Below are the key sources that I used for my analysis.")
+st.markdown("""
+- [OmniRide Developer Tools](https://www.omniride.com/contact/developer-tools/)
+- [Foothill Transit Developer Resources](https://www.foothilltransit.org/developer-resources)
+- [Rep. Torres — GovTrack Profile (CA District 35)](https://www.govtrack.us/congress/members/CA/35)
+- [California Senate District Mapping — District 35](https://sdmg.senate.ca.gov/committeehome/2025-congressional-districts/congressional-district-35-2025)
+""")
