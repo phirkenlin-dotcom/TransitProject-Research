937ad8919c985950d996d7fa37fd42dc136d6e6d
phirkenlin-dotcom <chinphirke@gmail.com>
2026-09-13 16:13:55 -0700

    Update 1_Transit_Research.py

--- pages/1_Transit_Research.py
+++ pages/1_Transit_Research.py
@@ -22,8 +22,20 @@ st.header("2. Geographic Map View")
 st.image("School with Bus Stop Map.jpg", caption="Map of Schools (Blue) and Transit Stops (Orange)")
 
 # st.header("3. Focus Area: Schools More Than 0.5 Miles Away")
-st.write("These schools face the greatest distance from public transit options.")
+st.write("I analyzed about 36 High Schools and Middle Schools in Congressional District 35.I observed that nearly 86% i.e.31 schools are within 0.5 miles of walking distrance")
+st.write("Whereas 5 schools face the greatest distance from public transit options.")
+col1, col2 = st.columns(2)
+
+with col1:
+       st.image("Schools By Walking Distances.jpg", caption="Overview of Schools by Distance of Transit Station")
+with col2:
+       st.image("Distance of Transit Station by School Name.jpg", caption="Distance of Transit Station by School Name")
+
+
 st.image("Schools With Bus Stops More Than 0.5 Miles away by School Name.jpg", caption="Schools with Bus Stops > 0.5 Miles Away")
+
+
+
 
 st.header("Data Sources")
 st.write("For this analysis I used various data which is publically available. Below are the key sources that I used for my analysis.")
