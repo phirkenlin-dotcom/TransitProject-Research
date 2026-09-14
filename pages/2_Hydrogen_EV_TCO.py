2dd70c56d00c8a6fe7ca4a722adeef27eec916da
phirkenlin-dotcom <chinphirke@gmail.com>
2026-09-13 22:26:46 -0700

    Update 2_Hydrogen_EV_TCO.py

--- pages/2_Hydrogen_EV_TCO.py
+++ pages/2_Hydrogen_EV_TCO.py
@@ -59,7 +59,7 @@ The model combines these components into a lifetime total for both types of buse
 
 
 ### Adjust Input Values & Result
-Users can adjust the parameters (purchase price, maintenance cost per mile, vehicle efficiency, electricity rates, and fuel costs) to simulate real-world scenarios and an understanding of how price changes can affect the prices of buses overall. Under typical market conditions ($0.15/kWh electricity vs. $8.00/kg hydrogen):
+Users can adjust the parameters (purchase price, maintenance cost per mile, vehicle efficiency, electricity rates, and fuel costs) to simulate real-world scenarios and an understanding of how price changes can affect the prices of buses overall. Under typical market conditions (\$0.15/kWh electricity vs. \$8.00/kg hydrogen):
 
 Battery Electric Bus (BEB) TCO: $1,487,500
 
