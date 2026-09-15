58b4cef19ba5141c95f46aba715946d5e0824f13
phirkenlin-dotcom <chinphirke@gmail.com>
2026-09-14 17:26:21 -0700

    Update home.py

--- home.py
+++ home.py
@@ -2,10 +2,12 @@ import streamlit as st
 st.set_page_config(page_title="Chinmayee Phirke", layout="wide")
 
 st.title("Hi, I'm Chinmayee Phirke")
-st.write("I'm a high school student interested in economic policy impact to, "
-         "envirionment and communities.")
+st.write("My work lies at the intersection of economics and data science.
+          I love using logic and math to solve real-world problems and I strive to understand how data and economic choices can improve systems and make a meaningful impact on our environment and communities. 
+          Through my school classes and independent projects, I've built data tools that turn complicated information into clear, useful solutions.
+        ")
 
-st.header("My Projects")
+st.subheader("My Projects")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -24,6 +26,5 @@ with col2:
     
 
 st.divider()
-st.header("About Me")
-st.write("[2-3 sentences about yourself — school, interests, how you got into this]")
+st.subheader("Connect with me")
 st.write("Connect with me on [LinkedIn](https://www.linkedin.com/in/chinmayee-phirke-4954ab39a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BL56aXZ58RVqZh7HU8cposA%3D%3D)")
