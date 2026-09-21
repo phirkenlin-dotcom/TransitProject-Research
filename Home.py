dd06289614241401f4b4dcb5cf2639a5d94c2eb4
phirkenlin-dotcom <chinphirke@gmail.com>
2026-09-20 22:01:23 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -21,11 +21,11 @@ with col2:
     
 
 st.divider()
-st.subheader("About Chinmayee Phirke")
+st.subheader("My Interests")
 st.write("""
           My work lies at the intersection of economics and data science.
           I love using logic and math to solve real-world problems and I strive to understand how data and economic choices can improve systems and make a meaningful impact on our environment and communities. 
           Through my school classes and independent projects, I've built data tools that turn complicated information into clear, useful solutions.
         """)
-st.subheader("Connect with me")
+st.subheader("Connect with Chinmayee Phirke")
 st.write("Connect with me on [LinkedIn](https://www.linkedin.com/in/chinmayee-phirke-4954ab39a/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BL56aXZ58RVqZh7HU8cposA%3D%3D)")
